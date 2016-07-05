1bf54a5826f24147fdc3246ab20fc3da0927cca6
Richard Shea <rshea@thecubagroup.com>
2016-07-05 20:17:24 +1200

    Initial cut of API call

--- jquery-when-testbed.js
+++ jquery-when-testbed.js
@@ -1,3 +1,22 @@
 $( document ).ready(function() {
     console.log('V0');
+    testWhenThen();
 });
+testWhenThen(){
+    var url = 'http://requestb.in/ukxas0uk'
+    arrJqxhr = $.get(url)
+                    .done(function(objIndicators, textStatus, jqXHR) {
+                        $.each( processedObjIndicators, function( key, value ) {
+                            console.log( key + ": " + value.id_tag );
+                        });
+                    })
+                    .fail(function() {
+                        console.log( "populateCurrentlySelectedIndicators : .fail fired" );
+                        console.log( "Error while fetching currently selected indicators." );
+                        alert(".fail within populateCurrentlySelectedIndicators: Please report this error");
+                    })
+                    .always(function() {
+                        console.log( "populateCurrentlySelectedIndicators : .always fired" );
+                    });
+
+}
